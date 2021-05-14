14bced176866e5f7e9f0f9611f9f64159b056710
dreampearl <rakhi.cs61@gmail.com>
2021-05-14 16:48:03 +0530

    Add indentation

--- calc.c
+++ calc.c
@@ -1,19 +1,18 @@
-#include <stdio.h>
-#include <math.h>
 #include "operation.h"
-
+#include <math.h>
+#include <stdio.h>
 
 void main() {
-	float a,b,res;
-	char c,s;
-	do{
-	    printf("please enter an operation\n");
-	scanf("%f",&a);
-	scanf(" %c",&c);
-	scanf("%f",&b);
-    operation(a,c,b);
- printf("do you want to continue\n");
- scanf(" %c",&s);
- printf("your input was %c",s);
-	}while(s=='y');
+    float a, b, res;
+    char c, s;
+    do {
+        printf("please enter an operation\n");
+        scanf("%f", &a);
+        scanf(" %c", &c);
+        scanf("%f", &b);
+        operation(a, c, b);
+        printf("do you want to continue\n");
+        scanf(" %c", &s);
+        printf("your input was %c", s);
+    } while (s == 'y');
 }
--- operation.c
+++ operation.c
@@ -1,44 +1,38 @@
-#include <stdio.h>
-#include <math.h>
 #include "operation.h"
+#include <math.h>
+#include <stdio.h>
 
-void operation(int a,char c,int b){
+void operation(int a, char c, int b) {
 
     float res;
 
-    switch(c)
-     {
-         case '+':
-         
-             res=a+b;
-             printf("%f\n",res);
-             break;
-         
-          case '-':
-         
-             res=a-b;
-             printf("%f\n",res);
-             break;
-         
-            case '*':
-         
-             res=a*b;
-             printf("%f\n",res);
-             break;
-         
-          case '/':
-         
-             res=a/b;
-             printf("%f\n",res);
-             break;
-         
-         default:
-         printf("operator not found\n");
-         break;
-         
+    switch (c) {
+    case '+':
 
-         
-     }
-  
+        res = a + b;
+        printf("%f\n", res);
+        break;
 
+    case '-':
+
+        res = a - b;
+        printf("%f\n", res);
+        break;
+
+    case '*':
+
+        res = a * b;
+        printf("%f\n", res);
+        break;
+
+    case '/':
+
+        res = a / b;
+        printf("%f\n", res);
+        break;
+
+    default:
+        printf("operator not found\n");
+        break;
+    }
 }
